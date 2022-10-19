e7e48ec56a8556e291e181be5981fd063da5cd7e
sagebeme <eudicas5k@gmail.com>
2022-10-19 14:45:36 -0700

    adding the other prototypes

--- main.h
+++ main.h
@@ -10,5 +10,9 @@ int _putstring(char *s);
 int print_number(int n);
 int print_digit(int n);
 int _printf(const char *format, ...);
+int dec_to_binary(int n);
+int rev_str(char *s);
+int rot13(char *point);
+int _strlen(char *s);
 
 #endif
