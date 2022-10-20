ef935a6aa4630624c44e4a775d756c4fbfd7b4dc
slykare <sylaninteriors@gmail.com>
2022-10-20 13:06:21 -0700

    change code

--- main.h
+++ main.h
@@ -1,18 +1,33 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
-int _putstring(char *s);
-int print_number(int n);
-int print_digit(int n);
 int _printf(const char *format, ...);
-int dec_to_binary(int n);
-int rev_str(char *s);
-int rot13(char *point);
-int _strlen(char *s);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif
+#endif  /* _MAIN_H */
+
